5940d3dd6b78c008b1e7a072de35e2b57fb1ec88
Kiruthigaa K <krithikaarajkumaar@gmail.com>
2025-11-17 01:43:05 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Version-1-Military-Surveillance/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
